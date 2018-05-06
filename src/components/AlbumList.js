b275395234feac87b8bedaaea835b4d015342ec2
ahmadabdolsaheb <ahmad.abdolsaheb@gmail.com>
2018-05-06 18:22:31 +0300

    render album titles

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,16 +1,24 @@
 // import libraries for making a Component
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
   componentWillMount() {
-    
-    console.log('componentWillMount');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
   }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text> {album.title} </Text>);
+  }
+
   render() {
+    console.log(this.state);
     return (
       <View>
-        <Text>Album Lists!!!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
