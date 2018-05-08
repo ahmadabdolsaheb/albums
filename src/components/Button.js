2f8a1c6d89bdf3540ae351c6b28c3af3c3acfcab
ahmadabdolsaheb <ahmad.abdolsaheb@gmail.com>
2018-05-08 14:40:21 +0300

    button styles added

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = (props) => {
+const Button = () => {
+  const { buttonStyle, textStyle } =  styles;
   return (
-    <TouchableOpacity>
-      <Text >
+    <TouchableOpacity style={buttonStyle}>
+      <Text style={textStyle}>
         "click"
       </Text>
     </TouchableOpacity>
@@ -12,14 +13,22 @@ const Button = (props) => {
 };
 
 const styles = {
-  containerStylo: {
-    borderBottomWidth: 1,
-    padding: 5,
+  buttonStyle: {
+    flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'flex-start',
-    flexDirection: 'row',
-    borderColor: '#ddd',
-    position: 'relative'
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  },
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
   }
 };
 
